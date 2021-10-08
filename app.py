6b31aceeaafbbbbca4a8fc70c912fcbf9334e368
jittima160549 <90383844+jittima160549@users.noreply.github.com>
2021-10-08 17:41:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = 'vXbAKKFXzW2g90YHtNI1RMtZcrEzeuYANImQAgqOMuEnpn6pXT/RhpN4EhSM5ZCAABWv1sk2HRbxK2HuaplOzHoxRSwNPFI0xc4xtZjuBzP480qDKH/FJT/NzmranUGAjXuMgOzuwSEm8Ju0MYkG2QdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
